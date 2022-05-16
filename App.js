404192d66861f70b0ef1bbc8ad7435bd4f1df87f
チョ・ジンイル <jojinil@chojinirus-MacBook-Air.local>
2022-05-16 22:12:39 +0900

    TIL

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import DateHead from './components/DateHead'
-import AddToDo from './components/AddToDo'
-import Empty from './components/Empty'
-import { StyleSheet } from 'react-native'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react';
+import DateHead from './components/DateHead';
+import AddToDo from './components/AddToDo';
+import Empty from './components/Empty';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
-  const today = new Date()
+  const today = new Date();
   return (
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.wrapper}edges={['bottom']}>{ /*props안에 배열형태로 선언, 하단 부분에만 SafeArea적용하겠다는 뜻*/ }
-          <DateHead date={today}/>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.wrapper} edges={['bottom']}>
+        <KeyboardAvoidingView
+          behavior={Platform.select({ios: 'padding'})}
+          style={styles.avoid}>
+          {/*props안에 배열형태로 선언, 하단 부분에만 SafeArea적용하겠다는 뜻*/}
+          <DateHead date={today} />
           <Empty />
           <AddToDo />
-        </SafeAreaView>
-      </SafeAreaProvider>
-  )
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: 'white',
   },
-})
+  avoid: {
+    flex: 1,
+  },
+});
--- components/AddToDo.js
+++ components/AddToDo.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import React, {useState} from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
 
 export default function AddToDo() {
-    return <View style={styles.wrapper} />
+  const [text, setText] = useState('');
+
+  return (
+    <View style={styles.wrapper}>
+      <TextInput
+        placeholder="할일을 입력하세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        height: 64,
-        backgroundColor: '#fcd734',
-    }
-})
+  wrapper: {
+    height: 64,
+    backgroundColor: '#fcd734',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16, // === paddingLeft: 16, paddingRight: 16
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 8, // === paddingTop: 8, paddingBottom: 8
+  },
+});
