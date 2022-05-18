cdd2846d685760bb16a408ac4696012d6a6392e4
チョ・ジンイル <jojinil@chojinirus-MacBook-Air.local>
2022-05-18 15:07:13 +0900

    test

--- components/AddToDo.js
+++ components/AddToDo.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput } from "react-native";
 
 export default function AddToDo() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   return (
     <View style={styles.wrapper}>
       <TextInput
-        placeholder="할일을 입력하세요."
+        placeholder="할일을 입력하세요!!!"
         style={styles.input}
         value={text}
         onChangeText={setText}
@@ -19,10 +19,10 @@ export default function AddToDo() {
 const styles = StyleSheet.create({
   wrapper: {
     height: 64,
-    backgroundColor: '#fcd734',
+    backgroundColor: "#fcd734",
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingHorizontal: 16, // === paddingLeft: 16, paddingRight: 16
   },
   input: {
